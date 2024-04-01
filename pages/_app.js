8165d532a19b9b8aab6334e1523c28c40556ff9e
Gustavo <gstvrrds@gmail.com>
2024-04-01 10:05:17 -0300

    feat:criação do primeiro context

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import MainContainer from "@/Components/MainContainer";
+export default function App({ Component, pageProps }) {
+  return (
+    <MainContainer>
+      <Component {...pageProps} />;
+    </MainContainer>
+  );
+}
---
+++ pages/otherpage.js
@@ -0,0 +1,9 @@
+export default function otherPage(){
+    return(
+        <div>
+            <h1>
+                Hello otherPage
+            </h1>
+        </div>
+    )
+}
